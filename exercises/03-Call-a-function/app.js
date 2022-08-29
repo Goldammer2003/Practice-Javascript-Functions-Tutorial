680acb514369630e65abc817214a82f4a914e9c4
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-08-29 20:49:10 +0000

    Did Exercise 3

--- exercises/03-Call-a-function/app.js
+++ exercises/03-Call-a-function/app.js
@@ -3,5 +3,12 @@ function calculateArea(length,edge)
 	return length * edge;
 }
 // Your code Below this line:
+function squareArea (length) 
+{
+	return length*length;
+}
+let squareArea1 =calculateArea (4,4);
 
+let squareArea2 =calculateArea (2,2);
 
+let squareArea3 =calculateArea (5,5);
